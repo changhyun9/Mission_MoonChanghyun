6735f165e134ea92f755757459edccc988546c7e
changhyun9 <ckdgus1713@naver.com>
2023-11-07 16:23:08 +0900

    feat : complete step 1

--- src/main/java/mission_1week/Main.java
+++ src/main/java/mission_1week/Main.java
@@ -5,9 +5,11 @@ import mission_1week.domain.request.RequestObject;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+            System.out.print("명령) ");
             String input = sc.nextLine();
             RequestObject requestObject = new RequestObject(input);
 
